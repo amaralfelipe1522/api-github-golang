a54c2558e329d06340b33ded733ca9be5ee42c35
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-20 10:49:49 -0300

    O arquivo client.go foi criado, assim como as estruturas bases para as respostas e requisições

---
+++ client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const (
+	defaultBasePath = "https://api.github.com/"
+	mediaType       = "application/vnd.github.v3+json"
+)
+
+// Client se refere a estrutura que irá gerenciar a comunicação com a API
+type Client struct {
+	client  *http.Client
+	BaseURL *url.URL
+}
+
+// Response é uma resposta da API que envolve a http.Response retornada da requisição
+type Response struct {
+	*http.Response
+}
+
+// ErrorResponse resporta um possível erro causado na requisição
+type ErrorResponse struct {
+	// http.Response que causou o erro
+	Response *http.Response
+	// Mensagem de erro
+	Message string
+}
+
+// Error implementa a interface error
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v", r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
+}
