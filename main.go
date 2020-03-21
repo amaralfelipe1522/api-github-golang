c3078015fe026bd8a69904a8f39d86a721b92767
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-21 11:18:45 -0300

    Lightly refactoring the code

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func main() {
+func get(url string) {
 	fmt.Println("Starting the application...")
-	response, err := http.Get("https://httpbin.org/ip")
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-	}
-	jsonData := map[string]string{"firstname": "Felipe", "lastname": "Amaral"}
-	jsonValue, _ := json.Marshal(jsonData)
-	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
@@ -27,4 +16,10 @@ func main() {
 		fmt.Println(string(data))
 	}
 	fmt.Println("Terminating the application...")
+}
+
+func main() {
+	//baseURL := "https://api.github.com"
+	userURL := "https://api.github.com/users/amaralfelipe1522/repos"
+	get(userURL)
 }
