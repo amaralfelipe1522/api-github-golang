de52b379993ca63384677cd7a24297e0f50d42f1
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-23 17:03:20 -0300

    Feeding a structure with some user data

--- main.go
+++ main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+// Usuario armazena a estrutura base de dados do usuario
+type Usuario struct {
+	Login    string `json:"login"`
+	Name     string `json:"name"`
+	QtdRepos int    `json:"public_repos"`
+	Bio      string `json:"bio"`
+	URL      string `json:"html_url"`
+}
 
 func get(url string) {
 	fmt.Println("Starting the application...")
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
+		fmt.Print(err.Error())
 	}
-	fmt.Println("Terminating the application...")
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var usuario Usuario
+	json.Unmarshal(responseData, &usuario)
+
+	fmt.Println(usuario)
+	defer fmt.Println("Terminating the application...")
 }
 
 func main() {
 	//baseURL := "https://api.github.com"
-	userURL := "https://api.github.com/users/amaralfelipe1522/repos"
+	userURL := "https://api.github.com/users/amaralfelipe1522"
 	get(userURL)
 }
